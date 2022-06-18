c89de3edd3c44001bf51b557cb357799bc6bb6f0
fedir72 <fedir72@ua.fm>
2022-06-18 23:25:20 +0300

    + collectionView and functionality for main viewcontroller

--- SpreadsheetsDataBase/SpreadsheetsDataBase/DataBase/Models/Sheet.swift
+++ SpreadsheetsDataBase/SpreadsheetsDataBase/DataBase/Models/Sheet.swift
@@ -13,6 +13,7 @@ extension Sheet {
     
     func getMainLayerOfSheet() -> Sheet {
         return self.filter { $0.parentId == "" }
+            .sorted { $0.type == .d || $1.type != .d }
     }
     
     func getChildItems(by parentId: String) -> Sheet {
