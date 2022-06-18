084b353947276684bf906c4d15d128a1caa59bff
fedir72 <fedir72@ua.fm>
2022-06-18 09:12:23 +0300

    + project file structure

---
+++ SpreadsheetsDataBase/SpreadsheetsDataBase/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpreadsheetsDataBase
+//
+//  Created by Fedii Ihor on 18.06.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
